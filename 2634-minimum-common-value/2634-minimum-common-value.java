096773d7d79ba1d334edd3eec43461a9b93e9e74
JyothiAyila <119840130+AyilaJyothi@users.noreply.github.com>
2025-02-09 21:45:20 +0530

    Time: 2 ms (86.14%), Space: 62.4 MB (78.64%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -1,23 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Set<Long> set = new HashSet<>();
-        Map<Long, Long> m = new TreeMap<>();
-
-        for (int num : nums1) {
-            set.add((long) num);
-        }
-        for (int num : nums2) {
-            if (set.contains((long) num)) {
-                m.put((long) num, 2L);
-            } else {
-                m.put((long) num, 1L);
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
             }
-        }
-        for (Map.Entry<Long, Long> ele : m.entrySet()) {
-            if (ele.getValue() == 2) {
-                return ele.getKey().intValue();
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else {
+                j++;
             }
         }
         return -1;
