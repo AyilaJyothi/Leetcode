927c12518f55234835433fa77ec05cc78f0173ce
JyothiAyila <119840130+AyilaJyothi@users.noreply.github.com>
2025-08-01 22:54:44 +0530

    Time: 17 ms (19.8%), Space: 54.8 MB (78.19%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0;
+        int j=people.length-1;
+        int count=0;
+        while(i<=j){
+            if(people[j]==limit){
+                count++;
+                j--;
+            }
+            else if(people[i]+people[j]<=limit){
+                count++;
+                i++;
+                j--;
+                if(i==j){
+                    count++;
+                    break;
+                }
+            }
+            else{
+                count++;
+                j--;
+            }
+        }
+        return count;
+    }
+}
